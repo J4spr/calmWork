8ff71a95fd833b18b9b21bae8f76d5b23927bfd6
J4spr <jasper07.verbruggen@gmail.com>
2023-05-30 12:27:59 +0200

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,17 @@
 function toggleIframe() {
   const iframe = document.querySelector(".hidden-iframe");
   iframe.style.display = iframe.style.display === "none" ? "block" : "none";
+  // iframe.src = ""
 }
 
-const elements = document.querySelectorAll(".changeGif");
-elements.forEach((element) => {
-    element.addEventListener("click", (e) => {
-        e.preventDefault();
-        changeBackground(element.id);
-    });
-});
+// const elements = document.querySelectorAll(".changeGif");
+// elements.forEach((element) => {
+//     element.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         changeBackground(element.id);
+//     });
+// });
 
-function changeBackground(type) {
+function changeTheme(type) {
   document.body.style.backgroundImage = `url('../assets/gifs/${type}.gif')`;
 }
