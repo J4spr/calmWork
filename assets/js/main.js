3ac0ff643882f12ddc4c6bef6a31afb56f9f34ab
J4spr <jasper07.verbruggen@gmail.com>
2023-05-30 19:09:27 +0200

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,24 +15,28 @@ function changeTheme(type) {
   document.body.style.backgroundImage = `url('../assets/gifs/${type}.gif')`;
   switch (type) {
     case "hiphop":
-      iframe.src =
-        "https://www.youtube.com/embed/videoseries?controls=0&amp;list=PL_UCDs2ps_71f_WxFiOGE6dGUqpq9hkeA";
+      myIframe.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/videoseries?controls=0&amp;list=PL_UCDs2ps_71f_WxFiOGE6dGUqpq9hkeA"
+      );
       break;
     case "ambient":
-      iframe.src =
-        "https://www.youtube.com/watch?v=s4ISiIFFLao&list=PL290F940EDA519EB6&ab_channel=OVERSEA";
+      myIframe.setAttribute(
+        "src",
+        "https://www.youtube.com/watch?v=s4ISiIFFLao&list=PL290F940EDA519EB6&ab_channel=OVERSEA");
       break;
     case "lofi":
-      iframe.src =
-        "https://www.youtube.com/watch?v=32CYcqFt1-8&list=PLUjbpfyneWKITlbjiGj3ybzeSqG1zl6Hl&ab_channel=ChillRadio";
+      myIframe.setAttribute(
+        "src",
+        "https://www.youtube.com/watch?v=32CYcqFt1-8&list=PLUjbpfyneWKITlbjiGj3ybzeSqG1zl6Hl&ab_channel=ChillRadio"
+      );
       break;
     case "phonk":
-      iframe.src =
-        "https://www.youtube.com/embed/videoseries?list=PLliiMqsVIF-OPMfR4mlwvdVLMp9ko0QKY";
+      myIframe.setAttribute("src", "https://www.youtube.com/embed/videoseries?list=PLliiMqsVIF-OPMfR4mlwvdVLMp9ko0QKY")
       break;
     case "index":
       window.location.href = "index.html";
       break;
   }
- 
 }
+// myIframe.setAttribute("src", "");
