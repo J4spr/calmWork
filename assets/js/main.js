760edb53e596b2692571158eca0623e8ed37b75a
Cédric <work@cedricverlinden.com>
2023-05-30 19:47:34 +0200

    Fixed Lo-Fi

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,35 +5,40 @@ function toggleVideo() {
 
 // const elements = document.querySelectorAll(".changeGif");
 // elements.forEach((element) => {
-  //     element.addEventListener("click", (e) => {
-    //         e.preventDefault();
-    //         changeBackground(element.id);
-    //     });
-    // });
-    
-    function changeTheme(type) {
-      document.body.style.backgroundImage = `url('../assets/gifs/${type}.gif')`;
-    const iframe = document.getElementById("youtube-video");
+//     element.addEventListener("click", (e) => {
+//         e.preventDefault();
+//         changeBackground(element.id);
+//     });
+// });
+
+function changeTheme(type) {
+  document.body.style.backgroundImage = `url('../assets/gifs/${type}.gif')`;
+  const iframe = document.getElementById("youtube-video");
   switch (type) {
     case "hiphop":
-      myIframe.setAttribute(
+      iframe.setAttribute(
         "src",
         "https://www.youtube.com/embed/videoseries?list=PL_UCDs2ps_71f_WxFiOGE6dGUqpq9hkeA"
       );
       break;
-    case "ambient":
-      myIframe.setAttribute(
-        "src",
-        "https://www.youtube.com/watch?v=s4ISiIFFLao&list=PL290F940EDA519EB6&ab_channel=OVERSEA");
-      break;
+    // WORKS
     case "lofi":
-      myIframe.setAttribute(
+      iframe.setAttribute(
         "src",
-        "https://www.youtube.com/watch?v=32CYcqFt1-8&list=PLUjbpfyneWKITlbjiGj3ybzeSqG1zl6Hl&ab_channel=ChillRadio"
+        "https://www.youtube.com/embed/videoseries?list=PLUjbpfyneWKITlbjiGj3ybzeSqG1zl6Hl"
+      );
+      break;
+    case "ambient":
+      iframe.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/videoseries?list=PL290F940EDA519EB6"
       );
       break;
     case "phonk":
-      myIframe.setAttribute("src", "https://www.youtube.com/embed/videoseries?list=PLliiMqsVIF-OPMfR4mlwvdVLMp9ko0QKY")
+      iframe.setAttribute(
+        "src",
+        "https://www.youtube.com/embed/videoseries?list=PLliiMqsVIF-OPMfR4mlwvdVLMp9ko0QKY"
+      );
       break;
     case "index":
       window.location.href = "index.html";
